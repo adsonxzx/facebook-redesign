de05fc90cf6e36622760ce6dfee965d557a72060
Adson Wesley <adsonxzx@gmail.com>
2019-11-15 10:15:56 -0300

    setting base of project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [ 
+          {loader: 'babel-loader'}, 
+        ],
+        exclude: '/node_modules/'
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ]
+  }
+}
